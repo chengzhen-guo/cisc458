2808e194f3986a1506ccbf9e4a6caf51e76760df
Chengzhen Guo <15cg53@queensu.ca>
2021-03-03 06:46:29 -0500

    Update TestAll.sh

--- testSuite-phase2/TestAll.sh
+++ testSuite-phase2/TestAll.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 for f in *.pt; do
-    echo "hello world"
-    echo "$f"
-    #ssltrace "ptc -o2 -t2 -L ../ptscr/lib/pt "$f".pt" ../ptscr/lib/pt/parser.def -e > test_aj_051.ssltrace-t2-e
+    echo "${f%%.*}"
+    #ssltrace "ptc -o2 -t2 -L ../ptscr/lib/pt $f.pt" ../ptscr/lib/pt/parser.def -e > $f.eOut
 done
