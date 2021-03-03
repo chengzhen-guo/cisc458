167272437e1f32efc9cfdeb550da8ee359519a3a
Chengzhen Guo <15cg53@queensu.ca>
2021-03-03 06:50:11 -0500

    Update TestAll.sh

--- testSuite-phase2/TestAll.sh
+++ testSuite-phase2/TestAll.sh
@@ -3,6 +3,6 @@ del *.eOut
 for i in *.pt
 do
     echo "=== pti $i ==="
-    ssltrace "ptc -o2 -t2 -L ../ptscr/lib/pt ${i%%.*}.pt" ../ptscr/lib/pt/parser.def -e > ${i%%.*}.eOut
+    ssltrace "ptc -o2 -t2 -L ../ptsrc/lib/pt ${i%%.*}.pt" ../ptsrc/lib/pt/parser.def -e > ${i%%.*}.eOut
 done
 echo "=== ==="
