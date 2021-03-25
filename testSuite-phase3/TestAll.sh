fc4c53b834b1807fbc6ac82137d819c92b8a529b
Chengzhen Guo <15cg53@queensu.ca>
2021-03-25 12:23:48 -0400

    Update TestAll.sh

--- testSuite-phase3/TestAll.sh
+++ testSuite-phase3/TestAll.sh
@@ -8,8 +8,8 @@ done
 for f in $(find . -name '*.pt')
 do
     i=${f#.}
-    echo "=== pti $i ==="    
-    ssltrace "ptc -o3 -t3 -L ../ptsrc/lib/pt ${i%%.*}.pt" ../ptsrc/lib/pt/semantic.def > .${i%%.*}.eOut
+    echo "=== pti $f ==="    
+    ssltrace "ptc -o3 -t3 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/semantic.def > .${i%%.*}.eOut
 done
 
 echo "=== Test Done. ==="
