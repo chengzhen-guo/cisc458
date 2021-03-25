44d21cc526b6768d077e061313c67180463d12b4
Chengzhen Guo <15cg53@queensu.ca>
2021-03-25 11:14:01 -0400

    Update TestAll.sh

--- testSuite-phase3/TestAll.sh
+++ testSuite-phase3/TestAll.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-find . -name '*.eOut' | xargs rm
 
-for i in *.pt
+for f in $(find . -name '*.eOut'); do rm $f
+
+for f in $(find . -name '*.pt')
 do
     echo "=== pti $i ==="
     #ssltrace "ptc -o3 -t3 -L ../ptsrc/lib/pt ${i%%.*}.pt" ../ptsrc/lib/pt/semantic.def > ${i%%.*}.eOut
