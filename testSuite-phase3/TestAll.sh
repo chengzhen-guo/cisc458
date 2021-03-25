69bccf9807406ae3213b2244e1328078fa7a4205
Chengzhen Guo <15cg53@queensu.ca>
2021-03-25 11:47:34 -0400

    Update TestAll.sh

--- testSuite-phase3/TestAll.sh
+++ testSuite-phase3/TestAll.sh
@@ -7,7 +7,7 @@ done
 
 for f in $(find . -name '*.pt')
 do
-    $i = ${f:1}
+    $i = "${f:1}"
     echo "=== pti $i ==="
     ssltrace "ptc -o3 -t3 -L ../ptsrc/lib/pt ${i%%.*}.pt" ../ptsrc/lib/pt/semantic.def > ${i%%.*}.eOut
 done
