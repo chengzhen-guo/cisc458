8a099d58fe195e824901811cbcac577e7d196f95
Chengzhen Guo <15cg53@queensu.ca>
2021-03-31 03:12:56 -0400

    code

---
+++ testSuite-phase4/TestCompile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for f in $(find . -name '*.eOut')
+do
+    rm $f
+done
+
+for f in $(find . -name '*.pt')
+do
+    i=${f#.}
+    echo "=== pti $f ==="    
+    ssltrace "ptc -o4 -t4 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/coder.def -e > .${i%%.*}.eOut
+done
+
+echo "=== Test Done. ==="
