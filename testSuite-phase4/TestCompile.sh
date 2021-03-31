a810a9a1cba3ee1fd81bcaca496e206e473ee61f
Chengzhen Guo <15cg53@queensu.ca>
2021-03-31 16:08:15 -0400

    Update TestCompile.sh

--- testSuite-phase4/TestCompile.sh
+++ testSuite-phase4/TestCompile.sh
@@ -9,8 +9,8 @@ for f in $(find . -name '*.pt')
 do
     i=${f#.}
     echo "=== pti $f ==="    
-    ptc -o4 -L ../ptsrc/lib/pt $f > .${i%%.*}.eOut
-    #ssltrace "ptc -o4 -t4 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
+    #ptc -o4 -L ../ptsrc/lib/pt $f > .${i%%.*}.eOut
+    ssltrace "ptc -o4 -t4 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
 done
 
 echo "=== Test Done. ==="
