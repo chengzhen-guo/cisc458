200d934872562716abc3f0861b332f59b4adad49
CISC 458 Group Account E <cisc458e@linux1.caslab.queensu.ca>
2021-04-01 16:47:08 -0400

    zero add

--- testSuite-phase4/string-assignment.s
+++ testSuite-phase4/string-assignment.s
@@ -10,3 +10,22 @@ ptmain:    pushl   %ebp
     .data    
 s18:    .asciz  "Hello"
     .text    
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    s18,%eax
+    pushl    %eax
+    call    pttrap15
+    addl    $4,%esp
+    movl    %eax,%esi
+    popl    %edx
+    popl    %ecx
+    popl    %ebx
+    popl    %eax
+    movl    %esi,%eax
+    movb    %al,%ebx
+    pushl    %ebx
+    call    pttrap0
+    leave    
+    ret    
--- testSuite-phase4/string-initialvalue.s
+++ testSuite-phase4/string-initialvalue.s
@@ -9,3 +9,23 @@ ptmain:    pushl   %ebp
     .data    
 s16:    .asciz  "Hello"
     .text    
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    s16,%eax
+    pushl    %eax
+    call    pttrap15
+    addl    $4,%esp
+    movl    %eax,%esi
+    popl    %edx
+    popl    %ecx
+    popl    %ebx
+    popl    %eax
+    movl    %esi,%eax
+    movb    %al,%ebx
+    pushl    %ebx
+    movl    $3,n
+    call    pttrap0
+    leave    
+    ret    
