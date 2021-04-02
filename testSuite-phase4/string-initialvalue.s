8b50d3d663ec2ef99d675f17d1266cfb160e20ca
CISC 458 Group Account E <cisc458e@linux1.caslab.queensu.ca>
2021-04-02 02:23:24 -0400

    Test string

---
+++ testSuite-phase4/string-initialvalue.s
@@ -0,0 +1,25 @@
+    .data    
+    .comm    u,260
+n:    .long    0
+    .text    
+    .globl    ptmain
+ptmain:    pushl   %ebp
+        movl    %esp, %ebp    
+    movl    $2,u+0
+    .data    
+s16:    .asciz  "Hello"
+    .text    
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    s16,%eax
+    pushl    %eax
+    lea    u+4,%eax
+    pushl    %eax
+    call    pttrap101
+    addl    $4,%esp
+    popl    %edx
+    popl    %ecx
+    popl    %ebx
+    popl    %eax
