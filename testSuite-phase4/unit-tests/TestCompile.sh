d409d2df17a5596dc3f0689d6416f4fd073935a9
Chengzhen Guo <15cg53@queensu.ca>
2021-04-06 02:16:57 -0400

    struct test

---
+++ testSuite-phase4/unit-tests/TestCompile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for f in $(find . -name '*.eOut')
+do
+    rm $f
+done
+
+for f in $(find . -name '*.pt')
+do
+    i=${f#.}
+    echo "=== pti $f ==="    
+    #ptc -o4 -L ../ptsrc/lib/pt $f > .${i%%.*}.eOut
+    ssltrace "ptc -o4 -t4 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
+done
+
+echo "=== Test Done. ==="
