d516e4d33fd50eaac4bbb937bdef13c5ce88f1a1
Chengzhen Guo <15cg53@queensu.ca>
2021-04-06 06:40:46 -0400

    ln

--- .vscode/GTEMP.pas
+++ .vscode/GTEMP.pas
@@ -1,13 +1,5 @@
 using output;
 
-var a = 1;
-var b = "Hello";
-var c = true;
+var x = "Hello world";
 
-var d : like 1;
-var e : like "Hello";
-var f : like true;
-
-var x : like a;
-var y : like b;
-var z : like c;
+put (x / 4 : 5); putln;
