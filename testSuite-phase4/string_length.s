43bda2da7ca587ad61b44ccc67f5b2852a60eff8
CISC 458 Group Account E <cisc458e@linux1.caslab.queensu.ca>
2021-04-05 23:15:06 -0400

    Test

---
+++ testSuite-phase4/string_length.s
@@ -0,0 +1,31 @@
+    .data    
+    .comm    u,264
+n:    .long    0
+    .text    
+    .globl    ptmain
+ptmain:    pushl   %ebp
+        movl    %esp, %ebp    
+    movl    $1,u+0
+    .data    
+s16:    .asciz  "Hello"
+    .text    
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    s16,%eax
+    pushl    %eax
+    lea    u+4,%eax
+    pushl    %eax
+    call    pttrap101
+    addl    $8,%esp
+    popl    %edx
+    popl    %ecx
+    popl    %ebx
+    popl    %eax
+    pushl    %eax
+    pushl    %ebx
+    pushl    %ecx
+    pushl    %edx
+    lea    u+4,%eax
+    pushl    %eax
