3e038482e6833c811d6542a4bc7b5d4c03bac936
Chengzhen Guo <15cg53@queensu.ca>
2021-04-06 02:19:31 -0400

    change folder

--- testSuite-phase4/system-tests/TestCompile.sh
+++ testSuite-phase4/system-tests/TestCompile.sh
@@ -10,7 +10,7 @@ do
     i=${f#.}
     echo "=== pti $f ==="    
     #ptc -o4 -L ../ptsrc/lib/pt $f > .${i%%.*}.eOut
-    ssltrace "ptc -o4 -t4 -L ../ptsrc/lib/pt $f" ../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
+    ssltrace "ptc -o4 -t4 -L ../../ptsrc/lib/pt $f" ../../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
 done
 
 echo "=== Test Done. ==="
