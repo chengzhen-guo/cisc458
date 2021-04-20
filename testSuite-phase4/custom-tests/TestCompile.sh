d18c779e54bd287dca25df767d9e42b03454c110
Chengzhen Guo <15cg53@queensu.ca>
2021-04-19 20:22:56 -0400

    Add test

---
+++ testSuite-phase4/custom-tests/TestCompile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for f in $(find . -name '*.eOut')
+do
+    rm $f
+done
+
+for f in $(find . -name '*.out')
+do
+    rm $f
+done
+
+for f in $(find . -name '*.pt')
+do
+    i=${f#.}
+    echo "=== pti $f ==="    
+    #ptc -o4 -L ../ptsrc/lib/pt $f > .${i%%.*}.eOut
+    ssltrace "ptc -o4 -t4 -L ../../ptsrc/lib/pt $f" ../../ptsrc/lib/pt/coder.def > .${i%%.*}.eOut
+done
+
+echo "=== Test Done. ==="
